e3fdfdee5d28ffe5ec60aa196047001915eab20c
Dre King <dreking11@gmail.com>
2021-09-11 14:28:30 -0400

    Sub Categories CRUD

--- controllers/category.js
+++ controllers/category.js
@@ -33,7 +33,7 @@ exports.update = async (req,res) => {
         );
         res.json(updated);
     } catch(err) {
-        res.status(400).send('Create Update Fail');
+        res.status(400).send('Category Update Fail');
     }
 };
 
@@ -42,6 +42,6 @@ exports.remove = async (req,res) => {
         const deleted = await Category.findOneAndDelete({slug: req.params.slug });
         res.json(deleted)
     } catch (err) {
-        res.status(400).send('Create Category Fail');
+        res.status(400).send('Category delete Fail');
     }
 };
