77dcc87443075b8b3fa674a742c6aefa49ac2d85
Dre King <dreking11@gmail.com>
2021-10-05 02:12:42 -0400

    Total Product endpoints

--- controllers/product.js
+++ controllers/product.js
@@ -16,6 +16,7 @@ exports.create = async(req,res) => {
     }
 };
 
+
 exports.listAll = async (req,res) => {
     let products = await Product.find({})
     .limit(parseInt(req.params.count))
@@ -57,3 +58,46 @@ exports.update = async (req,res) => {
         });
     }
 }
+
+// WITHOUT PAGNATION
+// exports.list = async (req,res) => {
+//     try {
+//       const {sort,order,limit} = req.body
+//       const products = await Product.find({})
+//       .populate('category')
+//       .populate('subs')
+//       .sort([[sort, order]])
+//       .limit(limit)
+//       .exec();
+
+//       res.json(products);
+//     } catch (err) {
+//         console.log(err)
+//     }
+// };
+
+exports.list = async (req,res) => {
+    try {
+      const {sort,order,page} = req.body;
+      const currentPage = page || 1
+      const perPage = 3
+
+      const products = await Product.find({})
+      .skip((currentPage - 1) * perPage)
+      .populate('category')
+      .populate('subs')
+      .sort([[sort, order]])
+      .limit(perPage)
+      .exec();
+
+      res.json(products);
+    } catch (err) {
+        console.log(err)
+    }
+};
+exports.productsCount = async (req,res) => {
+    let total = await Product.find({})
+    .estimatedDocumentCount()
+    .exec();
+    res.json(total);
+};
